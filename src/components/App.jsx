f75ed9bef5deb4d5905030250b936a333c101363
KZapal <kamil.zapal@o2.pl>
2024-01-14 21:33:24 +0100

    task 2 without random bgcolor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import user from 'folder/user.json';
+import data from 'folder/data.json';
 
 const App = () => {
   return (
-    <Profile
-      avatar={user.avatar}
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
+    <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,7 +12,7 @@ const Profile = ({
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        <img src={avatar} alt={username + 'avatar'} className={css.avatar} />
+        <img src={avatar} alt={username + ' avatar'} className={css.avatar} />
         <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
